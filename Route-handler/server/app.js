01e0be74c6677550b198d94b0e8cfb611a36fadb
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-05-17 14:58:43 +0545

    removed git corrupt folders

---
+++ Route-handler/server/app.js
@@ -0,0 +1,52 @@
+// Phase 2
+const {
+  getAllArtists,
+  getLatestArtist,
+  getArtistByArtistId,
+  addArtist,
+  editArtistByArtistId,
+  deleteArtistByArtistId,
+  getAlbumsForLatestArtist,
+  getAlbumsByArtistId,
+  getAlbumByAlbumId,
+  addAlbumByArtistId,
+  editAlbumByAlbumId,
+  deleteAlbumByAlbumId,
+  getFilteredAlbums,
+  getSongsByArtistId,
+  getSongsByAlbumId,
+  getSongBySongId,
+  addSongByAlbumId,
+  editSongBySongId,
+  deleteSongBySongId
+} = require('./data');
+
+const express = require('express');
+const app = express();
+
+// Your code here
+app.use(express.json());
+app.use((req,res,next)=>{
+  console.log('Request Body: ',req.body);
+  next();
+})
+app.get('/artists', (req, res) => {
+  res.status(200).send(getAllArtists());
+});
+app.post('/artists', (req, res) => {
+  const newArtist=addArtist(req.body);
+  res.status(201).send(newArtist);
+});
+app.get('/artists/latest', (req, res) => {
+  res.status(200).send(getLatestArtist());
+});
+app.get('/artists/latest/albums', (req, res) => {
+  res.status(200).send(getAlbumsForLatestArtist());
+});
+// DO NOT MODIFY
+if (require.main === module) {
+  const port = 8000;
+  app.listen(port, () => console.log('Server is listening on port', port));
+} else {
+  module.exports = app;
+}
