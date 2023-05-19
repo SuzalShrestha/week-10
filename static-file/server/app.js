a568827708f0bfcf83951ed861848d7f56c346f1
suzalshrestha <sujal.scxi121@amv.edu.np>
2023-05-19 17:23:36 +0545

    added bonus static files

--- static-file/server/app.js
+++ static-file/server/app.js
@@ -4,6 +4,7 @@ const app = express();
 // app.use('/',express.static('assets'));
 app.use('/',express.static('assets/scripts'));
 app.use('/stylesheets',express.static('assets/css'));
-
+app.use('stickers',express.static('assets/images'));
+//assets/images/hello.png is not found as it is not in the static folder but rather in the stickers folder
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
