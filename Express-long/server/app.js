0f98de21fdbb0614ed8b636704dfe21f451256ef
suzalshrestha <sujal.scxi121@amv.edu.np>
2023-05-20 07:49:26 +0545

    completed phase 2 express-long

---
+++ Express-long/server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+require('express-async-errors');
+const logger=(req,res,next)=>{
+  console.log(req.method,req.path);
+  res.on('finish',()=>{
+    console.log(res.statusCode);
+  });
+  next();
+};
+app.use(logger);
+// For testing purposes, GET /
+app.get('/', (req, res) => {
+  res.json("Express server running. No content provided at root level. Please use another route.");
+});
+
+// For testing express.json middleware
+app.post('/test-json', (req, res, next) => {
+  // send the body as JSON with a Content-Type header of "application/json"
+  // finishes the response, res.end()
+  res.json(req.body);
+  next();
+});
+
+// For testing express-async-errors
+app.get('/test-error', async (req, res) => {
+  throw new Error("Hello World!")
+});
+
+app.use('/static',express.static('assets'));
+app.use((req,res,next)=>{
+  res.status(404).json({error:"The requested resource could not be found."});
+});
+
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
