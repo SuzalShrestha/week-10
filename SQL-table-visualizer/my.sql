980928d2ba8ec1ecbe32d6b0e2caf85016be95dc
suzalshrestha <sujal.scxi121@amv.edu.np>
2023-05-22 07:15:10 +0545

    completed sql-table-visualizer

---
+++ SQL-table-visualizer/my.sql
@@ -0,0 +1,16 @@
+Table color{
+  id integer
+  name text
+}
+Table music{
+  id integer 
+  name  text
+  type_of_work text 
+  instrumental  boolean
+  composition_year  integer
+  composer  text
+  sample blob
+}
+
+
+Ref: "color"."id" < "music"."name"
