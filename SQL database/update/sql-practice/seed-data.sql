38c66f22a20dab62ce459969acfdf389b43c0287
suzalshrestha <sujal.scxi121@amv.edu.np>
2023-05-26 20:20:56 +0545

    completed update

---
+++ SQL database/update/sql-practice/seed-data.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS friends;
+
+CREATE TABLE friends (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO friends (first_name, last_name)
+VALUES
+  ('Ryan', 'Pond'),
+  ('Sky', 'Tyler'),
+  ('Morgan', 'Jones'),
+  ('Shannon', 'Noble'),
+  ('River', 'Song');
+
+SELECT * FROM friends;
+--Solution
+--Step 1
+UPDATE friends
+SET first_name='Ryder',
+    last_name='Pond'
+WHERE first_name='Ryan' AND last_name='Pond';
+--Step 2
+UPDATE friends
+SET last_name = 'Blue'
+WHERE first_name = 'Tyler'
+  AND last_name = 'Sky';
