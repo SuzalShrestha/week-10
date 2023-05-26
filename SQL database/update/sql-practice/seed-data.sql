90469c1f621bc30489df8c66a449409f6e976040
suzalshrestha <sujal.scxi121@amv.edu.np>
2023-05-26 20:26:51 +0545

    completed update bonus in sql

--- SQL database/update/sql-practice/seed-data.sql
+++ SQL database/update/sql-practice/seed-data.sql
@@ -21,8 +21,16 @@ UPDATE friends
 SET first_name='Ryder',
     last_name='Pond'
 WHERE first_name='Ryan' AND last_name='Pond';
+SELECT * FROM friends;
 --Step 2
 UPDATE friends
 SET last_name = 'Blue'
 WHERE first_name = 'Tyler'
-  AND last_name = 'Sky';
+AND last_name = 'Sky';
+SELECT * FROM friends;
+--Bonus
+UPDATE friends
+SET first_name='Empty'
+WHERE id=5;
+SELECT * FROM friends;
+
