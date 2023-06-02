5e69fb207065dfe1d5af234da5016eeca374afc1
suzalshrestha <sujal.scxi121@amv.edu.np>
2023-06-02 18:32:48 +0545

    completed subqueries in sql database

--- SQL database/Subqueries/sql-practice/example.sql
+++ SQL database/Subqueries/sql-practice/example.sql
@@ -1,3 +1,13 @@
 --subqueries for list of toys for a cat named Garfield
 SELECT toys.name FROM toys WHERE cat_id IN(
 SELECT id FROM cats WHERE name='Garfield');
+--dynamic insert using subqueries
+INSERT INTO toys(name,cat_id)
+SELECT 'Pepperoni',id FROM cats WHERE name='Garfield';
+--bonus
+--1
+INSERT INTO toys(name,cat_id)
+SELECT "Cat Bed",id FROM cats WHERE birth_year<2013;
+--2
+INSERT INTO cats_backup
+SELECT * FROM cats;
